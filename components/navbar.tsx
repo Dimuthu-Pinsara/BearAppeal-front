88a42d0b20329b76cd223fcc39662477e3b0e43e
Dimuthu-Pinsara <kdpjayathilakavta@gmail.com>
2025-11-27 01:27:19 +0530

    do1 : Implement navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -93,7 +93,7 @@ export function Navbar() {
                 className="relative text-gray-700 hover:text-gray-900 transition-colors"
               >
                 <ShoppingCart size={20} />
-                {cartCount > 0 && (
+                {cartCount >= 0 && (
                   <span className="absolute -top-2 -right-2 bg-black text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
                     {cartCount}
                   </span>
